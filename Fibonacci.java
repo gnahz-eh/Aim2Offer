f7ea6f2d8b0d59f4fb5125f29839fb37a136c227
He Zhang (Rudy) <425220727@qq.com>
2017-03-04 17:05:15 +0800

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Administrator on 2017/3/4.
+ */
+public class Fibonacci {
+
+    long fib(int n) {
+        long yesterday = 0;
+        long today = 1;
+        long tomorrow = 0;
+        for (int i = 2; i <= n; ++i) {
+            tomorrow = yesterday + today;
+            yesterday = today;
+            today = tomorrow;
+        }
+        return today;
+    }
+
+    public static void main(String[] args) {
+        Fibonacci f = new Fibonacci();
+        System.out.println(f.fib(7));// 13
+    }
+}
