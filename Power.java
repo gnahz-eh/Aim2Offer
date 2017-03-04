d86f09225968f4deb2464035a8c3e2d555fd225c
He Zhang (Rudy) <425220727@qq.com>
2017-03-04 18:30:02 +0800

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Administrator on 2017/3/4.
+ */
+public class Power {
+    public double myPow(double x, int n) {
+        if (n == 0) return 1;
+        if (n == Integer.MIN_VALUE && x > 1)
+            return 0;
+        if (n < 0) {
+            n = -n;
+            x = 1 / x;
+        }
+
+        return (n % 2 == 0) ? myPow(x*x, n/2) : x * myPow(x*x, n/2);
+    }
+
+    public static void main(String[] args) {
+        Power p = new Power();
+        System.out.println(p.myPow(0.2, 30));
+    }
+}
