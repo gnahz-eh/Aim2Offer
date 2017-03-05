1a04bbef00a7d9729c01bfc9c999804350a922d2
He Zhang (Rudy) <425220727@qq.com>
2017-03-05 17:25:35 +0800

    Add files via upload

---
+++ ReOrderArray.java
@@ -0,0 +1,33 @@
+/**
+ * Created by Administrator on 2017/3/5.
+ */
+public class ReOrderArray {
+
+    public void reOrderOddEven(int[] nums) {
+        if (nums == null) return;
+        int len = nums.length;
+
+        int left = 0;
+        int right = len - 1;
+
+        while (left != right) {
+            while (left < right && (nums[left] & 0x1) != 0)
+                left++;
+            while (left < right && (nums[right] & 0x1) == 0)
+                right--;
+            if (left < right) {
+                nums[left] = nums[left] ^ nums[right];
+                nums[right] = nums[left] ^ nums[right];
+                nums[left] = nums[left] ^ nums[right];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ReOrderArray r = new ReOrderArray();
+        int[] nums = {1,2,3,4,5};
+        r.reOrderOddEven(nums);
+        for (int e : nums)
+            System.out.print(e + " ");
+    }
+}
