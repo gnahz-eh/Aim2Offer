5f068c491b287bfdd8b47c0b7e477ab23061cb3b
He Zhang (Rudy) <425220727@qq.com>
2017-03-11 15:22:43 +0800

    Update 042_ReverseWordsInSequence.java

--- 042_ReverseWordsInSequence.java
+++ 042_ReverseWordsInSequence.java
@@ -17,3 +17,46 @@ public class ReverseWordsInSequence {
         r.reverseSequence(str);
     }
 }
+
+
+/**
+ * Created by Administrator on 2017/3/11.
+ */
+public class ReverseWordsInSequence {
+
+    public String reverseSequence(String str) {
+        if (str == null || str.length() <= 1)
+            return str;
+        StringBuilder sb = new StringBuilder(str);
+        reverse(sb, 0, str.length() - 1);
+        int begin = 0, end = 0;
+
+        while (end < str.length()) {
+            while (end < str.length() && sb.charAt(end) != ' ')
+                end++;
+            reverse(sb, begin, end - 1);
+            begin = end + 1;
+            end = begin;
+        }
+        return sb.toString();
+    }
+
+    public void reverse(StringBuilder sb, int start, int end) {
+        if (sb == null || end <= start)
+            return;
+
+        while (start < end) {
+            char temp = sb.charAt(start);
+            sb.setCharAt(start, sb.charAt(end));
+            sb.setCharAt(end, temp);
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        ReverseWordsInSequence r = new ReverseWordsInSequence();
+        String str = "i am a students.";
+        System.out.print(r.reverseSequence(str));
+    }
+}
