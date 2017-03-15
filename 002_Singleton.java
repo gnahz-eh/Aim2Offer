fbfadd989a3087639aca773f3fbd28304d7b3860
He Zhang (Rudy) <425220727@qq.com>
2017-03-15 10:28:46 +0800

    Create 002_Singleton.java

---
+++ 002_Singleton.java
@@ -0,0 +1,16 @@
+public class Singleton {
+    private volatile static Singleton unique;
+	
+	private Singleton() {}
+	
+	public static Singleton getInstance() {
+		if (unique == null) {                          // 检查实例，如果不存在，就进入同步区			
+			synchronized(Singleton.class) {            // 只有第一次才彻底执行这里的代码
+				if (unique == null) {                  // 进入区块后，再检查一次，如果仍然是null,才创建实例					
+					unique = new Singleton();
+				}
+			}
+		}
+		return unique;
+	}
+}
