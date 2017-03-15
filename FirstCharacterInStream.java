8440ff5f8f151c3c22662a9aa7580122d7becaf5
He Zhang (Rudy) <425220727@qq.com>
2017-03-15 14:30:12 +0800

    Add files via upload

---
+++ FirstCharacterInStream.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+/**
+ * Created by Administrator on 2017/3/15.
+ */
+public class FirstCharacterInStream {
+
+    public char firstAppearingOnce(String s) {
+        if (s == null)
+            return '#';
+        int[] hashMap = new int[256];
+        Arrays.fill(hashMap, -1);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (hashMap[s.charAt(i) - '0'] == -1)
+                hashMap[s.charAt(i) - '0'] = i;
+            else if (hashMap[s.charAt(i) - '0'] >= 0)
+                hashMap[s.charAt(i) - '0'] = -1;
+        }
+
+        int minIndex = 257;
+        int i = 0;
+        char res = '#';
+        for (; i < hashMap.length; i++) {
+            if (hashMap[i] >= 0 && hashMap[i] < minIndex) {
+                minIndex = hashMap[i];
+                res = (char)(i+'0');
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "google";
+        FirstCharacterInStream f = new FirstCharacterInStream();
+        System.out.print(f.firstAppearingOnce(s));
+    }
+}
