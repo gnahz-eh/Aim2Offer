6402c9f2863c0d593442c150b0cebf0f76ed36c3
He Zhang (Rudy) <425220727@qq.com>
2017-03-16 15:56:34 +0800

    Add files via upload

---
+++ MaxInSlidingWindow.java
@@ -0,0 +1,34 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MaxInSlidingWindow {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int len = nums.length;
+        if (len == 0) return nums;
+        int[] res = new int[len - k + 1];
+        
+        Deque<Integer> dq = new LinkedList<Integer>();
+        for (int i = 0; i < len; i++) {
+        	if (!dq.isEmpty() && dq.peek() < i - k + 1)
+        		dq.poll();
+        	
+        	while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()])
+        		dq.pollLast();
+        	
+        	dq.offer(i);
+        	if (i - k + 1 >= 0) 
+        		res[i - k + 1] = nums[dq.peek()];
+        }
+        
+        return res;
+    }
+    
+    public static void main(String[] args) {
+    	int[] nums = {2,3,4,2,6,2,5,1};
+    	MaxInSlidingWindow m = new MaxInSlidingWindow();
+    	int[] res = m.maxSlidingWindow(nums, 3);
+    	for (int i : res)
+    		System.out.print(i + " ");
+    }
+}
