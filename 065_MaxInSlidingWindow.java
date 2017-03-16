0afd2dfb1cf0f80b43b896d98bc30696e2aac011
He Zhang (Rudy) <425220727@qq.com>
2017-03-16 16:34:40 +0800

    Update 065_MaxInSlidingWindow.java

--- 065_MaxInSlidingWindow.java
+++ 065_MaxInSlidingWindow.java
@@ -3,6 +3,27 @@ import java.util.LinkedList;
 
 public class MaxInSlidingWindow {
 
+//     public int[] maxSlidingWindow(int[] nums, int k) {
+//         int len = nums.length;
+//         if (len == 0) return nums;
+//         int[] res = new int[len - k + 1];
+        
+//         Deque<Integer> dq = new LinkedList<Integer>();
+//         for (int i = 0; i < len; i++) {
+//         	if (!dq.isEmpty() && dq.peek() < i - k + 1)
+//         		dq.poll();
+        	
+//         	while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()])
+//         		dq.pollLast();
+        	
+//         	dq.offer(i);
+//         	if (i - k + 1 >= 0) 
+//         		res[i - k + 1] = nums[dq.peek()];
+//         }
+        
+//         return res;
+//     }
+    
     public int[] maxSlidingWindow(int[] nums, int k) {
         int len = nums.length;
         if (len == 0) return nums;
@@ -10,18 +31,18 @@ public class MaxInSlidingWindow {
         
         Deque<Integer> dq = new LinkedList<Integer>();
         for (int i = 0; i < len; i++) {
-        	if (!dq.isEmpty() && dq.peek() < i - k + 1)
-        		dq.poll();
-        	
-        	while (!dq.isEmpty() && nums[i] >= nums[dq.peekLast()])
+        	while (!dq.isEmpty() && dq.peekLast() < nums[i])
         		dq.pollLast();
         	
-        	dq.offer(i);
-        	if (i - k + 1 >= 0) 
-        		res[i - k + 1] = nums[dq.peek()];
+        	dq.offer(nums[i]);
+        	
+        	if (i >= k - 1) {
+        		res[i - k + 1] = dq.peek();
+        		if (nums[i - k + 1] == dq.peek())
+        			dq.pollFirst();
+        	}
         }
-        
-        return res;
+    	return res;
     }
     
     public static void main(String[] args) {
